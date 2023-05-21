arbruijn/dxx-redux

commit efcdd99f8cfd837b987608afed7d7d9a71e323d7

Free observatory stats memory after match
To prevent reported memory leaks after exiting the game.

d1/main/multi.c
@@ -480,6 +480,27 @@ void add_observatory_damage_stat(int player_num, fix shields_delta, fix new_shie
     }
 }
 
+void reset_observatory_stats() {
+	int i;
+	kill_event* ev;
+
+	for (i = 0; i < MAX_PLAYERS; i++) {
+		Last_kill[i] = NULL;
+		Last_death[i] = NULL;
+		Kill_streak[i] = 0;
+		while ((ev = Last_event[i]) != NULL) {
+			if (ev->prev != NULL) {
+				ev->prev->next = NULL;
+				Last_event[i] = ev->prev;
+			} else {
+				Last_event[i] = NULL;
+				First_event[i] = NULL;
+			}
+			d_free(ev);
+			ev = NULL;
+		}
+	}
+}
 
 void add_player_status(ubyte pnum, game_status status) {
 	status.next = NULL;
@@ -848,22 +869,8 @@ multi_new_game(void)
 	multi_received_objects = 0; 
 
 	// The observatory stats reset
-	kill_event* ev = NULL;
+	reset_observatory_stats();
 	for (i = 0; i < MAX_PLAYERS; i++) {
-		Last_kill[i] = NULL;
-		Last_death[i] = NULL;
-		Kill_streak[i] = 0;
-		while ((ev = Last_event[i]) != NULL) {
-			if (ev->prev != NULL) {
-				ev->prev->next = NULL;
-				Last_event[i] = ev->prev;
-			} else {
-				Last_event[i] = NULL;
-				First_event[i] = NULL;
-			}
-			d_free(ev);
-			ev = NULL;
-		}
 		add_observatory_stat(i, OBSEV_NONE);
 	}
 	Next_graph = 5;
@@ -1496,6 +1503,8 @@ multi_leave_game(void)
 	}
 
 	plyr_save_stats();
+
+	reset_observatory_stats();
 }
 		
 void 

d2/main/multi.c
@@ -490,6 +490,28 @@ void add_observatory_damage_stat(int player_num, fix shields_delta, fix new_shie
 	}
 }
 
+void reset_observatory_stats() {
+	int i;
+	kill_event* ev;
+
+	for (i = 0; i < MAX_PLAYERS; i++) {
+		Last_kill[i] = NULL;
+		Last_death[i] = NULL;
+		Kill_streak[i] = 0;
+		while ((ev = Last_event[i]) != NULL) {
+			if (ev->prev != NULL) {
+				ev->prev->next = NULL;
+				Last_event[i] = ev->prev;
+			} else {
+				Last_event[i] = NULL;
+				First_event[i] = NULL;
+			}
+			d_free(ev);
+			ev = NULL;
+		}
+	}
+}
+
 void add_player_status(ubyte pnum, game_status status) {
 	status.next = NULL;
 
@@ -873,22 +895,8 @@ multi_new_game(void)
 	game_disable_cheats();
 
 	// The observatory stats reset
-	kill_event* ev = NULL;
+	reset_observatory_stats();
 	for (i = 0; i < MAX_PLAYERS; i++) {
-		Last_kill[i] = NULL;
-		Last_death[i] = NULL;
-		Kill_streak[i] = 0;
-		while ((ev = Last_event[i]) != NULL) {
-			if (ev->prev != NULL) {
-				ev->prev->next = NULL;
-				Last_event[i] = ev->prev;
-			} else {
-				Last_event[i] = NULL;
-				First_event[i] = NULL;
-			}
-			d_free(ev);
-			ev = NULL;
-		}
 		add_observatory_stat(i, OBSEV_NONE);
 	}
 	Next_graph = 5;
@@ -1546,6 +1554,8 @@ multi_leave_game(void)
 				break;
 		}
 	}
+
+	reset_observatory_stats();
 }
 
 void

commit 72f9f6dcd07811c14ec3c63732242a55f5da85ac

Fix observer crash if host left game
Function reset_observatory_stats was called to soon, causing a crash in
the observer gauges code.

Fixes: efcdd99 ("Free observatory stats memory after match")

d1/main/inferno.c
@@ -459,6 +459,7 @@ int main(int argc, char *argv[])
 	newmenu_free_background();
 	free_mission();
 	PHYSFSX_removeArchiveContent();
+	reset_observatory_stats();
 
 	return(0);		//presumably successful exit
 }

d1/main/multi.c
@@ -1518,8 +1518,6 @@ multi_leave_game(void)
 	}
 
 	plyr_save_stats();
-
-	reset_observatory_stats();
 }
 		
 void 

d1/main/multi.h
@@ -668,4 +668,6 @@ extern kill_log_event* Kill_log;
 // Adds a damage stat for observatory UI.
 void add_observatory_damage_stat(int player_num, fix shields_delta, fix new_shields, fix old_shields, ubyte killer_type, ubyte killer_id, ubyte damage_type, ubyte source_id);
 
+void reset_observatory_stats();
+
 #endif /* _MULTI_H */

d2/main/inferno.c
@@ -494,6 +494,7 @@ int main(int argc, char *argv[])
 	newmenu_free_background();
 	free_mission();
 	PHYSFSX_removeArchiveContent();
+	reset_observatory_stats();
 
 	return(0);		//presumably successful exit
 }

d2/main/multi.c
@@ -1556,8 +1556,6 @@ multi_leave_game(void)
 				break;
 		}
 	}
-
-	reset_observatory_stats();
 }
 
 void

d2/main/multi.h
@@ -730,4 +730,6 @@ extern kill_log_event* Kill_log;
 // Adds a damage stat for observatory UI.
 void add_observatory_damage_stat(int player_num, fix shields_delta, fix new_shields, fix old_shields, ubyte killer_type, ubyte killer_id, ubyte damage_type, ubyte source_id);
 
+void reset_observatory_stats();
+
 #endif /* _MULTI_H */